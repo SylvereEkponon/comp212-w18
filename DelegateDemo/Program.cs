2239e6b908c0dd54445444aebdafbbd18e4072a7
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2018-01-23 14:27:42 -0500

    Delegate Demo

---
+++ DelegateDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    //STEP ONE
+    public delegate void MyDelegate();
+
+    class AnotherClass
+    {
+        public static void Clinton() { Console.WriteLine("Done By Clinton"); }
+
+        public  void Justin() { Console.WriteLine("Done By Justine"); }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //STEP THREE
+            MyDelegate Mydel = Eve;
+
+            Mydel = AnotherClass.Clinton;
+
+            Mydel = new AnotherClass().Justin;
+
+            //STEP FOUR
+            Mydel();
+        }
+
+        //STEP TWO  
+        static void Eve() { Console.WriteLine("Done by Eve"); }
+    }
+}
