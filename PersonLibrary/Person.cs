e2afd641fed8d40756cac70c13d47697d5573ce3
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2018-01-23 13:56:14 -0500

    Add project files.

---
+++ PersonLibrary/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonLibrary
+{
+    public class Person
+    {
+        static int MAX_PERSON = 5, PERSON_COUNT = 0;
+        public string FName { get; }
+        public string LName { get; }
+
+        Person(string fname, string lname)
+        {
+            FName = fname;
+            LName = lname;
+            PERSON_COUNT++;
+        }
+
+        public static Person CreatePerson(string fname, string lname)
+        {
+            if (PERSON_COUNT < MAX_PERSON)
+            {
+                return new Person(fname, lname);
+            }
+            throw new Exception("ERROR: Limit exceed");
+        } 
+
+        public override string ToString()
+        {
+            return $"{FName} {LName}";
+        }
+
+    }
+}
---
+++ PerxonTester/Program.cs
@@ -0,0 +1,27 @@
+﻿using PersonLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerxonTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = Person.CreatePerson("Donald", "Trump");
+            Console.WriteLine(person);
+
+            Person person2 = Person.CreatePerson("Solanky", "Taraja");
+            Console.WriteLine(person);
+            Console.WriteLine(Person.CreatePerson("Sylvere","Ekponon"));
+            person = Person.CreatePerson("Solanky", "Thareja");
+            person = Person.CreatePerson("Solanky", "Thareja");
+            person = Person.CreatePerson("Solanky", "Thareja");
+
+            //Person person3 = new Person("Solanky", "Thareja");
+        }
+    }
+}
